f38ccf4139f24fef38ad79d197fab5c4698e3125
Justin Stevens <justin.itrace@gmail.com>
2021-04-08 17:04:04 -0500

    optimize tailwindcss for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 module.exports = {
-  purge: [],
+  purge: {
+    // enabled: true,
+    content: ["./**/*.html"],
+    options: {
+      // Pass directly to PurgeCss options
+      // https://purgecss.com/configuration.html#options
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#49BBE9",
+          DEFAULT: "#1eace3",
+          dark: "#1996C8",
+        },
+        secondary: {
+          light: "#EE5994",
+          DEFAULT: "#e9327c",
+          dark: "#DE1767",
+        },
+      },
+      fontFamily: {
+        display: ["Rajdhani"],
+        sans: ["Roboto"],
+      },
+    },
   },
   variants: {
     extend: {
